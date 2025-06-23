ed657143cbfebdc718f8521f8e8c87ee20021735
Naveeth <naveethvkt7@gmail.com>
2025-06-23 17:01:10 +0530

    first problem

---
+++ p001.py
@@ -0,0 +1,16 @@
+"""
+Write a program to get a number print if its divisible by 5?
+Don't use % 
+
+Run 1:
+Enter any integer: 40
+Yes, 40 is divisible by 5
+
+Run 2:
+Enter any integer: 75
+Yes, 75 is divisible by 5
+
+Run 3:
+Enter any integer: 41
+No, 41 is divisible by 5
+"""
